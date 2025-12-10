e622353fc213683742f685b5aa947f08fb02d74d
Alexandre Negrel <alexandre@negrel.dev>
2025-12-10 21:01:43 +0100

    add nix package

--- flake.nix
+++ flake.nix
@@ -35,6 +35,28 @@
               MPSC_INCLUDE = "${mpsc.packages.${system}.default}/include";
             };
           };
+          packages = {
+            default = derivation {
+              name = "eventloop.h";
+              version = "0.1.0";
+              system = system;
+              builder = "${pkgs.bash}/bin/bash";
+              args = [
+                "-e"
+                "-c"
+                ''
+                  export PATH="$PATH:${pkgs.coreutils}/bin:${pkgs.bash}/bin:${pkgs.gawk}/bin:${pkgs.gnugrep}/bin"
+                  bash ${./build} bundle
+                  mkdir -p $out/include
+                  cp ./eventloop.h $out/include/eventloop.h
+                ''
+              ];
+
+              REPO = ./.;
+              THREADPOOL_INCLUDE = "${threadpool.packages.${system}.default}/include";
+              MPSC_INCLUDE = "${mpsc.packages.${system}.default}/include";
+            };
+          };
         }
       );
     in
